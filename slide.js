33e925ed33935e340875c49222f8e97be6507c44
Shaquille <Shaquillesadii@gmail.com>
2018-11-08 21:44:42 +0100

    Portfolio Upload

---
+++ slide.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById('overlay', 'mezelf', 'periode', 'gallerij', 'contact', 'opleiding').style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById('overlay', 'mezelf', 'periode', 'gallerij', 'contact', 'opleiding').style.width = "0%";
+}
+
