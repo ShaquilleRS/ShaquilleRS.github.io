18963a54d633f222e3c98df56f0c4e0eb61920e3
ShaquilleRS <32331928+ShaquilleRS@users.noreply.github.com>
2019-02-08 00:06:41 +0100

    Update slide.js

--- slide.js
+++ slide.js
@@ -10,11 +10,6 @@ function sendForm() {
     alert("Je bericht is verzonden, bedankt!")
 }
 
-var naam = prompt("Wat is je naam?");
-
-document.getElementById("welkom").innerHTML =
-    "Welkom " + naam + "";
-
 while (!naam) {
     var naam = prompt("Wat is je naam?");
 
@@ -22,17 +17,6 @@ while (!naam) {
         "Welkom " + naam + "";
 }
 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('klok').innerHTML =
-        h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
 function checkTime(i) {
     if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
     return i;
